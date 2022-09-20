6501bce5701c9d9ce61b6988e1e670fc319bc6ac
basar <basar@LAPTOP-40QODCLQ>
2022-09-20 16:56:56 +0300

    4 commit

---
+++ conditionals/src/conditionals/Main.java
@@ -0,0 +1,16 @@
+package conditionals;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int deger = 20;
+		if (deger > 20) {
+			System.out.println("Sayınız 20'den büyüktür");
+		} else if (deger < 20) {
+			System.out.println("Sayınız 20'den küçüktür");
+		} else {
+			System.out.println("Sayınız 20'ye eşittir");
+		}
+
+	}
+}

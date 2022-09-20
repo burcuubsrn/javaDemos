425f3a4c2b08879d6ff5da1aa907df8cb0719a0f
basar <basar@LAPTOP-40QODCLQ>
2022-09-20 16:51:51 +0300

    commit

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,14 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi1 = 50;
+		double ondalikli = 18.6;
+		String giris = ("Hoşgeldiniz");
+		boolean ruzgarDustuMu = false;
+		char karakter = 'A';
+
+	}
+
+}

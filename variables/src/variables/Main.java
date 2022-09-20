07325b72650840d8fc4fdc6c981b04c86f03b7ed
basar <basar@LAPTOP-40QODCLQ>
2022-09-20 17:16:29 +0300

    2 commit

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String sinifmevcudu = ("Sınıf Mevcudu: ");
+		int ogrenciSayisi = 35;
+
+		System.out.println(sinifmevcudu + ogrenciSayisi);
+		System.out.println(sinifmevcudu + ogrenciSayisi);
+		System.out.println(sinifmevcudu + ogrenciSayisi);
+
+	}
+
+}
